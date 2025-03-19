fe71ef9b8aad37a62a32ae1ce49d0ea1cf4ef435
[Meharazul islam] <[itsmeharazz@gmail.com]>
2025-03-20 03:03:45 +0600

    initialise project

---
+++ src/assets/data.js
@@ -0,0 +1,103 @@
+import logo from './Logo.png';
+import hero_1 from './hero__1.webp';
+import hero_2 from './hero__2.webp';
+import hero_3 from './hero__3.webp';
+import category_1 from './category__1.webp'
+import category_2 from './category__2.webp'
+import category_3 from './category__3.webp'
+import category_4 from './category__4.webp'
+import category_5 from './category__5.webp'
+import category_6 from './category__6.webp'
+import category_7 from './category__7.webp'
+import category_8 from './Soft drink gategory.png'
+import banner_1 from './banner__1.webp';
+import banner_2 from './banner__2.webp';
+import feature_1 from './feature__1.webp';
+import feature_2 from './feature__2.webp';
+import products_1 from './post__1.webp';
+import products_2 from './post__2.webp';
+import products_3 from './post__3.webp';
+import products_4 from './post__4.webp';
+import products_5 from './product__5.webp';
+
+export const assets = {
+    logo,
+    hero_1,
+    hero_2,
+    hero_3,
+    banner_1,
+    banner_2,
+    feature_1,
+    feature_2
+
+} 
+export const category_list = [
+  {
+    menu_name: "Fresh Fruits",
+    image: category_1,
+    stock: "9 Products",
+  },
+  {
+    menu_name: "Fresh Vags",
+    image: category_2,
+    stock: "8 Products",
+  },
+  {
+    menu_name: "Canned Goods",
+    image: category_3,
+    stock: "9 Products",
+  },
+  {
+    menu_name: "Fresh Fruits",
+    image: category_4,
+    stock: "9 Products",
+  },
+  {
+    menu_name: "Fresh Fruits",
+    image: category_5,
+    stock: "9 Products",
+  },
+  {
+    menu_name: "Eggs & Dary",
+    image: category_6,
+    stock: "7 Products",
+  },
+  {
+    menu_name: "Soft Drinks",
+    image: category_7,
+    stock: "9 Products",
+  },
+  {
+    menu_name: "Soft Drinks",
+    image: category_8,
+    stock: "9 Products",
+  },
+];
+
+export const products = [
+    {
+        image: products_1,
+        products_name:'Dried Mange',
+        Price:"500"
+    },
+    {
+        image: products_2,
+        products_name:'Chruncy Crisps',
+        Price:"300"
+    },
+    {
+        image: products_3,
+        products_name:'Jewel Cranberries',
+        Price:"200"
+    },
+    {
+        image: products_4,
+        products_name:'Almond organic',
+        Price:"500"
+    },
+    {
+        image: products_5,
+        products_name:'Dried Mange',
+        Price:"500"
+    },
+]
