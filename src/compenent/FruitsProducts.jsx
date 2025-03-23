f2b131bbebe3d58725c0285ee81777c44d9ebcad
[Meharazul islam] <[itsmeharazz@gmail.com]>
2025-03-23 23:18:42 +0600

    add products

---
+++ src/compenent/FruitsProducts.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { assets,products } from "../assets/data";
+import ProductCard from './ProductCard';
+
+const FruitsProducts = () => {
+  return (
+    <div className='container pt-16'>
+      <div className='lg:flex justify-between items-center'>
+        <div className=''>
+          <h3 className='font-semibold text-2xl'>Fruits & Vegetables</h3>
+          <p className='text-gray-600 mt-2'>
+            Buy farm fresh fruits and vegetable online at the best prices
+          </p>
+        </div>
+        <div className='space-x-4 mt-8 lg:mt-0'>
+          <button className='feature_btn'>Fruits</button>
+          <button className='text-gray-600 hover:text-green-700'>
+            Vegetables
+          </button>
+          <button className='text-gray-600 hover:text-green-700'>
+            Bread & Bakery
+          </button>
+        </div>
+      </div>
+      <div className='grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 pt-8 gap-5'>
+        <div className=''>
+          <img src={assets.feature_1} alt='' className='w-full h-full object-cover' />
+        </div>
+        {products.slice(0, 4).map((item) => (
+          <ProductCard
+            key={item.id}
+            img={item.image}
+            name={item.products_name}
+            price={item.Price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FruitsProducts
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import HeroSection from '../compenent/HeroSection';
 import Category from '../compenent/Category';
+import FruitsProducts from '../compenent/FruitsProducts';
+import BreakfastProducts from '../compenent/BreakfastProducts';
 
 const HomePage = () => {
      const [category, setCategory] = useState("All");
@@ -8,6 +10,8 @@ const HomePage = () => {
     <div>
         <HeroSection />
         <Category categoryes={category} setCategory={setCategory}/>
+        <FruitsProducts />
+        <BreakfastProducts />
     </div>
   )
 }
